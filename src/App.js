5872f045d8a3a4f73121239ab554cd8cfc80a65f
SujalRathore08 <sujalrathore2017@gmail.com>
2024-07-05 15:27:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
+
+function App() {
+  console.log("Hello",process.env.REACT_APP_RAPID_API_KEY);
+  return (
+    <BrowserRouter>
+      <Box sx={{backgroundColor:'#000'}}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Feed/>}/>
+          <Route path="*" exact element={<Feed/>}/>
+          <Route path="/video/:id" element={<VideoDetail/>} />
+          <Route path="/channel/:id" element={<ChannelDetail/>} />
+          <Route path="/search/:searchTerm" element={<SearchFeed/>} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+}
+
+export default App;
